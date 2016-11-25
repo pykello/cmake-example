00cd87251be0e37a306fdbe6e17f5ca2bcfe531d
Hadi Moshayedi <hadi@moshayedi.net>
2016-11-25 05:10:21 -0500

    first commit

---
+++ Demo/demo.cxx
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "../Hello/hello.h"
+using namespace std;
+
+int main() {
+ cout << multiply_by_2(4) << endl;
+}
+
---
+++ Hello/hello.cxx
@@ -0,0 +1,5 @@
+#include "hello.h"
+
+int multiply_by_2(int a) {
+ return 2 * a;
+}
